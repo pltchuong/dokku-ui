2b89e9d6d03c98cdce118389b3d71f6146c06e14
Phan Chuong <pltchuong@gmail.com>
2016-06-01 17:03:53 -0700

    build autoscroll

--- client/app/apps/builds/builds.controller.js
+++ client/app/apps/builds/builds.controller.js
@@ -8,17 +8,16 @@ class BuildsController {
     this.$location = $location;
     this.ModalService = ModalService;
     this.fetch();
-
-    var controller = this;
-    setInterval(function() {
-      controller.fetch();
-    }, 1000);
   }
 
   fetch() {
     if(this.$stateParams.app) {
       this.$http.get('/api/activities/' + this.$stateParams.build).then(response => {
         this.activity = response.data;
+
+        setTimeout(() => {
+          this.fetch();
+        }, 2000);
       });
     } else {
       this.$location.path('/apps');
