dc22c3e98442492b98b7044f78ae339ccd82f20f
Phan Chuong <pltchuong@gmail.com>
2016-06-17 20:02:57 -0700

    support git rev

--- client/app/apps/activities/activities.controller.js
+++ client/app/apps/activities/activities.controller.js
@@ -13,6 +13,15 @@ class ActivitiesController {
     if(this.$stateParams.app) {
       this.$http.get('/api/apps/' + this.$stateParams.app + '/activities').then(response => {
         this.activities = response.data;
+        this.activities.forEach(function(activity) {
+          activity.commands.forEach(function(command, i) {
+            activity.commands[i] = command.trim();
+            var rev = command.match(/^git-build ([0-9a-z]{40})$/);
+            if(rev && rev[1]) {
+              activity.rev = rev[1];
+            }
+          });
+        });
       });
     } else {
       this.$location.path('/apps');
