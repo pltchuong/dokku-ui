359aae6ca8deea1a92702ca15473794f2b085cc7
Phan Chuong <pltchuong@gmail.com>
2016-07-08 11:24:14 -0700

    fix small input when select collaborators

--- client/app/modals/edit-collaborators/edit-collaborators.controller.js
+++ client/app/modals/edit-collaborators/edit-collaborators.controller.js
@@ -2,9 +2,10 @@
 
 class ModalEditCollaboratorsController {
 
-  constructor(close, $http, collaborators) {
+  constructor(close, $http, $timeout, collaborators) {
     this.exit = close;
     this.$http = $http;
+    this.$timeout = $timeout;
     this.inputs = {
       collaborators: collaborators
     };
@@ -15,9 +16,57 @@ class ModalEditCollaboratorsController {
     this.$http
       .get('/api/users')
       .then((response) => {
-        this.inputs.users = response.data;
+        this.inputs.users = response.data.filter((user) => {
+          return !(this.inputs.collaborators.find((collaborator) => {
+            return collaborator.username === user.username;
+          }));
+        });
       })
     ;
+  }
+
+  resize($scope) {
+    setTimeout(() => {
+      // copied from select.js to fix small input issue
+      var ctrl = $scope.$select,
+        sizeWatch = null,
+        updaterScheduled = false,
+        input = ctrl.searchInput[0],
+        container = ctrl.searchInput
+          .parent()
+          .parent()[0],
+        calculateContainerWidth = function() {
+          // Return the container width only if the search input is visible
+          return container.clientWidth * !!input.offsetParent;
+        },
+        updateIfVisible = function(containerWidth) {
+          if (containerWidth === 0) {
+            return false;
+          }
+          var inputWidth = containerWidth - input.offsetLeft - 10;
+          if (inputWidth < 50) inputWidth = containerWidth;
+          ctrl.searchInput.css('width', `${inputWidth}px`);
+          return true;
+        };
+
+      ctrl.searchInput.css('width', '10px');
+      this.$timeout(() => { //Give tags time to render correctly
+        if (sizeWatch === null && !updateIfVisible(calculateContainerWidth())) {
+          sizeWatch = $scope.$watch(angular.noop, () => {
+            if (!updaterScheduled) {
+              updaterScheduled = true;
+              $scope.$$postDigest(() => {
+                updaterScheduled = false;
+                if (updateIfVisible(calculateContainerWidth())) {
+                  sizeWatch();
+                  sizeWatch = null;
+                }
+              });
+            }
+          });
+        }
+      });
+    }, 500);
   }
 
   close() {
